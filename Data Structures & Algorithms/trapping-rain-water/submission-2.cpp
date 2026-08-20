8fb46318ceb396bf9490312a84e1597529d1c454
Aman Kulwal <87664541+aman123424@users.noreply.github.com>
2026-08-20 17:30:26 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        int l = 0;
+        int r = 1;
+
+        // while(r < n) {
+        //     if(height[r] < height[l]) break;
+        //     r++;
+        //     l++;
+        // }
+
+        int totalWater = 0;
+
+        //Count from left
+        while(r < n) {
+            int blacks = 0;
+            while(r < n && height[r] < height[l]){
+                blacks += height[r++];
+            }
+
+            if(r == n) break;
+            
+            totalWater += height[l] * (r - l - 1) - blacks;
+            l = r;
+            r++;
+        }
+
+        int limit = l;
+        r = n - 1;
+        l = n - 2;
+
+        // while(l >= 0) {
+        //     if(height[l] < height[r]) break;
+        //     l--;r--;
+        // }
+
+        while(l >= limit) {
+            int blacks = 0;
+
+            while(l >= limit && height[r] > height[l]){
+                blacks += height[l--];
+            }
+
+            if(l < limit) break;
+
+            totalWater += height[r] * (r - l - 1) - blacks;
+            r = l;
+            l--;
+        } 
+
+        return totalWater;
+    }
+};
